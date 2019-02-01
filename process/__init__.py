9a0a76a63b3d31beada352f13a2f4a70c7493ac6
Côme MISCOPEIN SALER <45457627+ComeMS@users.noreply.github.com>
2019-02-01 14:27:06 +0100

    process.nearbyStations redefined

--- process/__init__.py
+++ process/__init__.py
@@ -1,17 +1,16 @@
-def nearbyStations(radius, positionGPSCoordinates):
+def nearbyStations(positionGPSCoordinates):
   """
     finds the stations around the given position and the max distance of search
     Parameters:
     -----------
-        radius: float
-            the max distance where to find a station
-        positionGPSCoordinates: tuple (x,y)
+        
+        positionGPSCoordinates: tuple (lat,long)
 
 
     Return:
     -------
-    list: tuples of stations' coordinates (x,y) sorted by ascendent distance
-      [(x1,y1), (x2,y2)...]  
+    list: Coordinates of nerby station (lat,long)
+      [(lat,long)] 
   """
  pass
 
